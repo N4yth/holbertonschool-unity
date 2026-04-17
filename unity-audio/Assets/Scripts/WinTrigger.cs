b431ff14505ca18c78485a534815463f9d808cbd
N4yt <80832294+N4yth@users.noreply.github.com>
2026-04-17 08:33:42 +0200

    feat : add win song

--- unity-audio/Assets/Scripts/WinTrigger.cs
+++ unity-audio/Assets/Scripts/WinTrigger.cs
@@ -4,15 +4,44 @@ using UnityEngine.UI;
 public class WinTrigger : MonoBehaviour
 {
 
-    public Text TimerText;
-    public Timer script;
+    [Tooltip("Script player movement")]
+    [SerializeField]
+    public PlayerController PlayerScript;
+
+    [Tooltip("Script camera movement")]
+    [SerializeField]
+    public CameraController CameraScript;
+
+    [Tooltip("Timer to pause")]
+    [SerializeField]
+    public Timer TimeScript;
+
+    [Tooltip("Timer to pause")]
+    [SerializeField]
+    public PauseMenu PauseScript;
+
+    [Tooltip("SFX to shut")]
+    [SerializeField]
+    public GameObject SFX;
+
+    [Tooltip("SFX to shut")]
+    [SerializeField]
+    private Animator animator;
+
     public GameObject WinPanel;
     [SerializeField] private AudioSource BGM;
+    [SerializeField] private AudioSource WinBGM;
 
     void OnTriggerEnter(Collider other)
     {
-        script.enabled = false;
+        animator.SetBool("isRunning", false);
+        animator.SetBool("isFalling", false);
+        CameraScript.enabled = false;
+        PlayerScript.enabled = false;
+        TimeScript.enabled = false;
+        PauseScript.enabled = false;
         WinPanel.gameObject.SetActive(true);
         BGM.Stop();
+        WinBGM.Play();
     }
 }
