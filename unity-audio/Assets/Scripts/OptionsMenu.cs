f3688b0be9429139f0eaf3044be55e1c2a137ead
N4yt <80832294+N4yth@users.noreply.github.com>
2026-04-19 16:29:03 +0200

    feat : add changeable music in option menu

--- unity-audio/Assets/Scripts/OptionsMenu.cs
+++ unity-audio/Assets/Scripts/OptionsMenu.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using System;
+using UnityEngine.Audio;
 
 public class OptionsMenu  : MonoBehaviour
 {
@@ -11,20 +12,33 @@ public class OptionsMenu  : MonoBehaviour
     public Button ApplyButton;
     public bool isInvert;
     public Toggle toggleButton;
+    private float oldvalue;
+    [SerializeField] private Slider bgmSlider;
+    [SerializeField] private AudioMixer mixer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    { 
-       PlayerPrefs.SetInt("ActualScene", 1);
-       isInvert = PlayerPrefs.GetInt("isInvert", 0) == 1;
-       toggleButton.isOn = isInvert;
-       BackButton.onClick.AddListener(Back);
-       ApplyButton.onClick.AddListener(Apply);
+    {
+        PlayerPrefs.SetInt("ActualScene", 1);
+
+        isInvert = PlayerPrefs.GetInt("isInvert", 0) == 1;
+        toggleButton.isOn = isInvert;
+
+        BackButton.onClick.AddListener(Back);
+        ApplyButton.onClick.AddListener(Apply);
+
+        float savedVolume = PlayerPrefs.GetFloat("BGMVolume", 1f);
+        bgmSlider.value = savedVolume;
+        oldvalue = savedVolume;
+        
+
+        bgmSlider.onValueChanged.AddListener(SetVolume);
     }   
 
     public void Back()
     {
         SceneManager.LoadScene(sceneBuildIndex: PlayerPrefs.GetInt("PreviouScene"));
+        bgmSlider.value = oldvalue;
     }
 
     public void Apply()
@@ -33,5 +47,15 @@ public class OptionsMenu  : MonoBehaviour
             PlayerPrefs.SetInt("isInvert", 1);
         else
             PlayerPrefs.SetInt("isInvert", 0);
+        oldvalue = bgmSlider.value;
+        Back();
+    }
+
+    public void SetVolume(float value)
+    {
+        float volume = Mathf.Log10(value) * 20;
+        mixer.SetFloat("BGMVolume", volume);
+
+        PlayerPrefs.SetFloat("BGMVolume", value);
     }
 }
